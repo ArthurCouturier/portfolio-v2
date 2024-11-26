d7be9fe0fc3f826fedafd0d7facde14641ce2df2
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2024-11-26 17:22:08 +0100

    fix: :bug: fixed bug of scrolling in the circle menu

--- src/components/ExpandCircle/ExpandCircle.tsx
+++ src/components/ExpandCircle/ExpandCircle.tsx
@@ -4,16 +4,29 @@ import './ExpandCircle.css';
 export default function ExpandCircle(props: { circleState: string; setCircleState: React.Dispatch<React.SetStateAction<string>> }) {
     useEffect(() => {
         if (props.circleState === 'opening') {
-            console.log('Circle is expanding');
+            document.documentElement.style.overflow = 'hidden';
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.documentElement.style.overflow = '';
+            document.body.style.overflow = '';
         }
+
+        return () => {
+            document.documentElement.style.overflow = '';
+            document.body.style.overflow = '';
+        };
     }, [props.circleState]);
+
 
     return (
         (props.circleState === 'opening' || props.circleState === 'closing') && (
             <div
                 className="absolute inset-0 flex justify-center items-center z-0"
             >
-                <div className={`bg-[#2f2e6e] rounded-full ${props.circleState === 'opening' ? 'animate-expandCircle' : 'animate-shrinkCircle'}`}></div>
+                <div
+                    className={`bg-[#2f2e6e] rounded-full ${props.circleState === 'opening' ? 'animate-expandCircle' : 'animate-shrinkCircle'
+                        }`}
+                ></div>
             </div>
         )
     );
